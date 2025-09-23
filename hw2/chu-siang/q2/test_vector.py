13a9c36e53bb5e9285d90eecf0aa6cf1be28c254
chu-siang <k32720101@gmail.com>
2025-09-23 12:25:10 +0000

    add additional test case

--- hw2/chu-siang/q2/test_vector.py
+++ hw2/chu-siang/q2/test_vector.py
@@ -3,9 +3,9 @@ import pytest
 import _vector
 
 def test_zero_length_vector():
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         _vector.angle(0.0, 0.0, 1.0, 0.0)
-    with pytest.raises(Exception):
+    with pytest.raises(ValueError):
         _vector.angle(1.0, 0.0, 0.0, 0.0)
 
 def test_zero_angle():
@@ -13,6 +13,9 @@ def test_zero_angle():
 
 def test_right_angle():
     assert _vector.angle(1.0, 0.0, 0.0, 3.0) == pytest.approx(math.pi/2, rel=1e-12, abs=1e-12)
+
+def test_opposite_angle():
+    assert _vector.angle(1.0, 0.0, -1.0, 0.0) == pytest.approx(math.pi, rel=1e-12, abs=1e-12)
 
 def test_other_angle():
     assert _vector.angle(1.0, 1.0, 1.0, 0.0) == pytest.approx(math.pi/4, rel=1e-12, abs=1e-12)

3d349a5885fad383327ea2fc5cdf3dfe754fb950
Kuishinboy987 <j30878014.cs14@nycu.edu.tw>
2025-09-23 22:19:10 +0800

    first submission for ans. of q1 q2

---
+++ hw2/Kuishinboy987/q1/Line.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <vector>
+
+class Line
+{
+public:
+    Line();
+
+    Line(size_t size);
+
+    Line(const Line&);
+    Line(Line&&) noexcept;
+    Line& operator=(const Line&);
+    Line& operator=(Line&&) noexcept;
+
+    ~Line();
+
+    size_t size() const;
+    
+    float const& x(size_t it) const;
+    float& x(size_t it);
+    float const& y(size_t it) const;
+    float& y(size_t it);
+
+private:
+    std::vector<float> xs;
+    std::vector<float> ys;
+};
---
+++ hw2/Kuishinboy987/q2/test_angle.py
@@ -0,0 +1,22 @@
+import math
+import math
+import pytest
+import vector_angle
+
+def test_same_direction():
+    assert math.isclose(vector_angle.vector_angle(1, 0, 2, 0), 0.0, rel_tol=1e-9)
+
+def test_opposite_direction():
+    assert math.isclose(vector_angle.vector_angle(1, 0, -1, 0), math.pi, rel_tol=1e-9)
+
+def test_perpendicular():
+    assert math.isclose(vector_angle.vector_angle(1, 0, 0, 1), math.pi/2, rel_tol=1e-9)
+
+def test_general_case():
+    angle = vector_angle.vector_angle(1, 1, 1, 0)
+    expected = math.pi / 4
+    assert math.isclose(angle, expected, rel_tol=1e-9)
+
+def test_zero_vector():
+    with pytest.raises(ValueError):
+        vector_angle.vector_angle(0, 0, 1, 0)

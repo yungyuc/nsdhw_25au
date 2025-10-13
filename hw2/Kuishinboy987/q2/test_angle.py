82335749f47fa603645438867b4ad570550fc987
Kuishinboy987 <j30878014.cs14@nycu.edu.tw>
2025-10-13 19:54:02 +0800

    adjust the broken layout

--- hw2/Kuishinboy987/q2/test_angle.py
+++ hw2/Kuishinboy987/q2/test_angle.py
@@ -3,7 +3,7 @@ import math
 import pytest
 import _vector as vector_angle
 
-def test_same_direction():
+def test_same_direction(): 
     assert math.isclose(vector_angle.vector_angle(1, 0, 2, 0), 0.0, rel_tol=1e-9)
 
 def test_opposite_direction():
